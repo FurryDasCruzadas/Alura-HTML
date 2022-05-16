3a80b4a0227b3731cfd9ba1aec6f4e9118baeec0
João Camilo M <jcamilomallmann@hotmail.com>
2022-05-16 17:08:40 -0300

    .

--- Javascript Basico/loops.js
+++ Javascript Basico/loops.js
@@ -1,10 +1,9 @@
 const Salvador = 1;
 const SãoPaulo = 2;
 
-
 const ListaDeDestino = new Array(`SãoPaulo`, `Salvador`);
 
-ListaDeDestino.push(`Rio Grande`)
+ListaDeDestino.push(`Rio Grande`);
 
 const IdadeComprador = 18;
 let Acomp = false;
@@ -14,15 +13,19 @@ let contador = 0;
 
 console.log("\nDestinos");
 console.log(ListaDeDestino);
-const Comprar = (IdadeComprador >= 18  || Acomp == true );
+const Comprar = IdadeComprador >= 18 || Acomp == true;
 
-while(contador<3){
-  if(ListaDeDestino[contador] == destino){
-    console.log("Destino existe")
+while (contador < 3) {
+  if (ListaDeDestino[contador] == destino) {
+    console.log("Destino existe");
     break;
   }
   contador++;
-
 }
 
-
+for (let i = 0; i < 3; i++) {
+  if (ListaDeDestino[i] == destino) {
+    console.log("teste existe");
+    break;
+  }
+}
