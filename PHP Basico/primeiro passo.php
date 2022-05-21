513676cafbaa37c6c1405f153c0c5d8a757497a9
João Camilo M <jcamilomallmann@hotmail.com>
2022-05-21 17:08:12 -0300

    .

--- PHP Basico/primeiro passo.php
+++ PHP Basico/primeiro passo.php
@@ -1,8 +1,18 @@
 <?php
 
-  $idade = 0.1;
-  $idade10Anos = 0.2;
-  $idadeFinal = 0.1 + 0.2;
+  $idade = 2;
+  $idade10Anos = 4;
+  $idadeFinal = $idade**$idade10Anos;
+  //$idadeFinal = $idade+$idade10Anos;
+  //$idadeFinal = $idade/$idade10Anos;
+  //$idadeFinal = $idade-$idade10Anos;
+  //$idadeFinal = ($idade*$idade10Anos) + 1;
+  $texto = "frase";
+  $tipo = gettype($idade);
 
+  //echo 'então seu tipo é ' . $tipo . ' mas vs é uma ' . $texto;
 
-  echo $idadeFinal;
+  echo "então seu tipo é $tipo\nmas vs é uma $texto\n";
+
+  echo "então seu tipo é $tipo" . PHP_EOL;
+  echo "as vs é uma $texto";
