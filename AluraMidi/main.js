9e418c9e5e10ca81bd3379688bd2963d670e7444
João Camilo M <jcamilomallmann@hotmail.com>
2022-06-01 16:34:15 -0300

    .

--- AluraMidi/main.js
+++ AluraMidi/main.js
@@ -1,18 +1,33 @@
-function tocaSom (ZUM) {
-  document.querySelector(ZUM).play();
+function tocaSom(Seletor) {
+  const elementos = document.querySelector(Seletor);
+  
+  if (elementos != null && elementos.localName == 'audio'){
+    elementos.play();
+  }
+  else{
+    console.log("Valor Inserido Invalido");
+  }
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
 //para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`; //template string
 
   tecla.onclick = function () {
-      tocaSom(idAudio);
-  }
+    tocaSom(idAudio);
+  };
 
+  tecla.onkeydown = function (Valor) {
+     if (Valor == 'Space' || Valor == 'Enter') {
+      tecla.classList.add("ativa");
+  }
+}
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("remove");
+  };
 }
