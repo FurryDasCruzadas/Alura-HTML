eccbda4dfbc932038562ee51a7a1c1e5e96216c5
João Camilo M <jcamilomallmann@hotmail.com>
2022-05-31 21:04:09 -0300

    ..

--- AluraMidi/main.js
+++ AluraMidi/main.js
@@ -1,5 +1,18 @@
-function tocarsomPOM() {
-  document.querySelector('#som_tecla_pom').play();
+function tocaSom (ZUM) {
+  document.querySelector(ZUM).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocarsomPOM;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; //template string
+
+  tecla.onclick = function () {
+      tocaSom(idAudio);
+  }
+
+}
