714c82adacd9f44687277c97ffa43194e6aeeb4f
João Camilo M <jcamilomallmann@hotmail.com>
2022-07-12 17:51:09 -0300

    ...

--- .py
+++ .py
@@ -1,13 +1,30 @@
-import numpy as np
-import matplotlib.pyplot as plot
-from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-axes=plot.axes(projection="3d")
+fig = plt.figure(figsize=(6,6))
 
-x_data=np.arange(0,50,0.1)
-y_data=np.arange(0,50,0.1)
-z_data=np.sin(x_data) * np.cos(y_data)
+ax = fig.add_subplot(111, projection='3d')
 
-axes.plot(x_data,y_data,z_data)
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
 
-plot.show()
+VecStart_start = [0,0,0,0]
+VecEnd_x = [1,0,0,0]
+VecEnd_y = [0,1,0,0]
+VecEnd_z = [0,0,1,0]
+for i in range(4):
+  ax.plot([VecStart_start[i], VecEnd_x[i]], [VecStart_start[i],VecEnd_y[i]],[VecStart_start[i],VecEnd_z[i]])
+
+
+VecStart_startx = [3,3,3,0]
+VecStart_starty = [0,0,0,0]
+VecStart_startz = [0,0,0,0]
+
+VecEnd_x1 = [VecStart_startx[0]+1,VecStart_startx[1],VecStart_startx[2],VecStart_startx[3]]
+VecEnd_y1 = [VecStart_starty[0],VecStart_starty[1]+1,VecStart_starty[2],VecStart_starty[3]]
+VecEnd_z1 = [VecStart_startz[0],VecStart_startz[1],VecStart_startz[2]+1,VecStart_startz[3]]
+for i in range(4):
+  ax.plot([VecStart_startx[i], VecEnd_x1[i]], [VecStart_starty[i],VecEnd_y1[i]],[VecStart_startz[i],VecEnd_z1[i]])
+
+plt.show()
--- ifsul.py
+++ ifsul.py
@@ -1,32 +1,27 @@
+from cmath import pi
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-fig = plt.figure(figsize=(4,4))
-
-ax = fig.add_subplot(111, projection='3d')
-
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("Z")
+from pytransform3d import rotations as pr
 
 
-VecStart_start = [0,0,0]
-VecEnd_x = [1,0,0]
-VecEnd_y = [0,1,0]
-VecEnd_z = [0,0,1]
-for i in range(3):
-  ax.plot([VecStart_start[i], VecEnd_x[i]], [VecStart_start[i],VecEnd_y[i]],zs=[VecStart_start[i],VecEnd_z[i]])
+ax = pr.plot_basis(R=np.eye(3), ax_s=4, lw=3) #ponto 1
+axis = 0
+angle = np.pi
 
-VecStart_start = [3,0,3]
-VecEnd_x = [6,4,0]
-VecEnd_y = [4,6,0]
-VecEnd_z = [4,4,0]
 
-for i in range(3):
-  ax.plot([VecStart_start[i], VecEnd_x[i]], [VecStart_start[i],VecEnd_y[i]],zs=[VecStart_start[i],VecEnd_z[i]])
+p = np.array([2,0,0])#ponto 2
+euler = [0, 0, 0]
+#euler[axis] = angle
+R = pr.active_matrix_from_intrinsic_euler_zyx(euler)
+pr.plot_basis(ax, R, p, lw=3)
+
+
+p = np.array([2,3,0])#ponto 3
+euler = [0, 0, pi]
+#euler[axis] = angle
+R = pr.active_matrix_from_intrinsic_euler_xyz(euler)
+pr.plot_basis(ax, R, p,lw=3)
+
 
 
 plt.show()
-
-
-plt.close()
